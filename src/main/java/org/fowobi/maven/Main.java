0c9bf5e66e539441494c4d7200280d8be2ae0937
Oluwatosin Olufowobi <fowobideveloper@gmail.com>
2025-08-05 09:54:30 +0100

    initial commit

---
+++ src/main/java/org/fowobi/maven/Main.java
@@ -0,0 +1,24 @@
+package org.fowobi.maven;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "hello docker";
+    }
+}
